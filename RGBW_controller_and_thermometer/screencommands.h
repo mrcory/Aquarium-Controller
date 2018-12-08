512964fbd7c587215521b8cff009c58cb1445a66
mrcory <33401433+mrcory@users.noreply.github.com>
2018-12-08 10:49:39 -0500

    Remove refresh control.

--- RGBW_controller_and_thermometer/screencommands.h
+++ RGBW_controller_and_thermometer/screencommands.h
@@ -1,4 +1,5 @@
-//Contains functions for display
+//Contains functions for OLED display
+//No updates intended
 
 #define colon ':' //Saving a little space
 
@@ -106,57 +107,50 @@ void showTemp(int _posX,int _posY) { //0,0
 }
 
 
-void drawHBar(int locX, int locY, int height, int width, int percent) { //Draw a progrss bar
-  int aLocX = map(percent, 1, 100, 1, width);
-  display.drawLine(locX, locY, locX + width, locY, WHITE); //Draw outline of progress bar
-  display.drawLine(locX, locY + height, locX + width, locY + height, WHITE); //Draw lower outline of progress bar
-
-  if (locX - (locX + aLocX > 1) ) { //If value is 0 then don't fill the bar
-    for (int i = 0; i < height; i++) { //Draw body of progress bar
-      display.drawLine(locX, locY + i, locX + aLocX, locY + i, WHITE);
+  void drawHBar(int locX, int locY, int height, int width, int percent) { //Draw a progrss bar
+    int aLocX = map(percent, 1, 100, 1, width);
+    display.drawLine(locX, locY, locX + width, locY, WHITE); //Draw outline of progress bar
+    display.drawLine(locX, locY + height, locX + width, locY + height, WHITE); //Draw lower outline of progress bar
+  
+    if (locX - (locX + aLocX > 1) ) { //If value is 0 then don't fill the bar
+      for (int i = 0; i < height; i++) { //Draw body of progress bar
+        display.drawLine(locX, locY + i, locX + aLocX, locY + i, WHITE);
+      }
     }
+  
+  }
+  
+  void ledStatus(int _X, int _Y) { //Show LED value bars <location X, location Y>
+    display.setTextSize(1);
+    display.drawChar(_X, _Y, 'R', WHITE, BLACK, 1); //Bar label
+    display.setCursor(_X+8, _Y); display.print(ledC[0]); //Display value digits
+    drawHBar(_X,_Y + 8, 3, 25, map(ledC[0], 0, 255, 0, 100)); //Testing progress bar
+  
+    display.drawChar(_X+30,_Y, 'G', WHITE, BLACK, 1); //Bar label
+    display.setCursor(_X+38, _Y); display.print(ledC[1]); //Display value digits
+    drawHBar(_X+30,_Y + 8, 3, 25, map(ledC[1], 0, 255, 0, 100)); //Testing progress bar
+  
+    display.drawChar(_X+60,_Y, 'B', WHITE, BLACK, 1); //Bar label
+    display.setCursor(_X+68,_Y); display.print(ledC[2]); //Display value digits
+    drawHBar(_X+60,_Y+ 8, 3, 25, map(ledC[2], 0, 255, 0, 100)); //Testing progress bar
+  
+    display.drawChar(_X+90,_Y, 'W', WHITE, BLACK, 1); //Bar label
+    display.setCursor(_X+98,_Y); display.print(ledC[3]); //Display value digits
+    drawHBar(_X+90,_Y + 8, 3, 25, map(ledC[3], 0, 255, 0, 100)); //Testing progress bar
+  
+    display.drawChar(0,_Y + 15, 'P', WHITE, BLACK, 1);
+    drawHBar(_X+9,_Y + 18, 3, 51, map(ledP, 0, 255, 0, 100));
+  
+    display.drawChar(_X+65,_Y + 15, 'B', WHITE, BLACK, 1);
+    drawHBar(_X+74,_Y + 18, 3, 51, map(ledC[4], 0, 255, 0, 100));
+  }
+  
+  void screenSetup() { //Do all screen setup in a single function
+   #if screenOLED
+    display.begin(SSD1306_SWITCHCAPVCC, displayAddress); //Initialize with I2C address
+   #endif
+   display.setTextColor(WHITE); //Set text color so it is visible
+    delay(250); //Give some time for the temp probe to start
   }
 
-}
-
-void ledStatus(int _X, int _Y) { //Show LED value bars <location X, location Y>
-  display.setTextSize(1);
-  display.drawChar(_X, _Y, 'R', WHITE, BLACK, 1); //Bar label
-  display.setCursor(_X+8, _Y); display.print(ledC[0]); //Display value digits
-  drawHBar(_X,_Y + 8, 3, 25, map(ledC[0], 0, 255, 0, 100)); //Testing progress bar
-
-  display.drawChar(_X+30,_Y, 'G', WHITE, BLACK, 1); //Bar label
-  display.setCursor(_X+38, _Y); display.print(ledC[1]); //Display value digits
-  drawHBar(_X+30,_Y + 8, 3, 25, map(ledC[1], 0, 255, 0, 100)); //Testing progress bar
-
-  display.drawChar(_X+60,_Y, 'B', WHITE, BLACK, 1); //Bar label
-  display.setCursor(_X+68,_Y); display.print(ledC[2]); //Display value digits
-  drawHBar(_X+60,_Y+ 8, 3, 25, map(ledC[2], 0, 255, 0, 100)); //Testing progress bar
-
-  display.drawChar(_X+90,_Y, 'W', WHITE, BLACK, 1); //Bar label
-  display.setCursor(_X+98,_Y); display.print(ledC[3]); //Display value digits
-  drawHBar(_X+90,_Y + 8, 3, 25, map(ledC[3], 0, 255, 0, 100)); //Testing progress bar
-
-  display.drawChar(0,_Y + 15, 'P', WHITE, BLACK, 1);
-  drawHBar(_X+9,_Y + 18, 3, 51, map(ledP, 0, 255, 0, 100));
-
-  display.drawChar(_X+65,_Y + 15, 'B', WHITE, BLACK, 1);
-  drawHBar(_X+74,_Y + 18, 3, 51, map(ledC[4], 0, 255, 0, 100));
-}
-
-void screenSetup() { //Do all screen setup in a single function
- #if screenOLED
-  display.begin(SSD1306_SWITCHCAPVCC, displayAddress); //Initialize with I2C address
- #endif
- display.setTextColor(WHITE); //Set text color so it is visible
-  delay(250); //Give some time for the temp probe to start
-}
-
-//Refresh rate controller
-bool fpsControl(int _rate) {
-  if (timer(1000/_rate,4)) {
-    return true;
-  } else {
-    return false;
-  }
 }
