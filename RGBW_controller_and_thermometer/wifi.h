83540505d1495eb5f1d9b31545a544056fca742a
mrcory <33401433+mrcory@users.noreply.github.com>
2018-12-21 20:26:58 -0500

    First wifi parts added

--- RGBW_controller_and_thermometer/wifi.h
+++ RGBW_controller_and_thermometer/wifi.h
@@ -1,1 +1,25 @@
-//Oh Yeah, we are gonna get some wifi in here. (Once I hve the hardware)
+//Oh Yeah, we are gonna get some wifi in here. (Once I have the hardware)
+
+char mySSID;
+char myPassword;
+
+
+//Some Supporting functions
+
+#if serialCommands
+  void wifiSSID(_ssid) {
+    mySSID = _ssid; //Set SSID
+    configSave(); //Save the config
+  }
+
+  void wifiPass(_password) {
+    myPassword = _password; //Set password
+    configSave(); //Save the config
+  }
+
+  cmdAdd("ssid",wifiSSID);
+  cmdAdd("wifipass",wifiPass);
+
+#else
+  #warning "Serial Commands are required to setup wifi."
+#endif
