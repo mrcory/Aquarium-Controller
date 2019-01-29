5a2d2615dbd6d5398f6417b6d29e351682623476
mrcory <33401433+mrcory@users.noreply.github.com>
2019-01-29 13:25:10 -0500

    New Icon

--- RGBW_controller_and_thermometer/tft.h
+++ RGBW_controller_and_thermometer/tft.h
@@ -15,6 +15,7 @@ void screenReset(int _X, int _Y, float _SIZE) {
 
 
 void activeDisplay() {
+  
   static bool flipFlop = true; //ledStatus will update every other refresh.
   
   if (fpsControl(2000)) { //Set the refresh rate in milliseconds
@@ -31,15 +32,19 @@ void activeDisplay() {
     ledStatus(0,39);
     
     screenReset(72,100,1);
-    
+
+    /*
     if (ledState == 1) { 
       display.print(F("ON ")); //needs to be 3 characters long to clear the screen
         } else {
       display.print(F("OFF"));
       }
-  
+    */
+    
     flipFlop = !flipFlop;
   }
+
+  
   
   #if tempWarnEnable //If temperature warning is enabled draw a warning icon.
     warnIcon(97,18);
@@ -49,22 +54,24 @@ void activeDisplay() {
   
   if (screenFirstRun == true) {
     screenReset(0,100,1);
-    display.print(F("LED Status: "));
+    //display.print(F("LED Status: "));
     screenFirstRun = false;
   }
 
 #if wifiEnable
   static bool oldBlynk = false;
   if (Blynk.connected() == true && oldBlynk == false) {
-    blynkLogo(100,60,"0x2632");
+    blynkLogo(130,90,0x2632);
     oldBlynk = true;
     } else {
   if (Blynk.connected() == false && oldBlynk == true) {
-    blynkLogo(100,60,ST77XX_BLACK);
+    blynkLogo(130,90,ST77XX_BLACK);
     oldBlynk = false;
   }
   }
 #endif
   
   }
+
+  //aniBitmap(20,75,1250,ST7735_RED,ST7735_WHITE);
 }
