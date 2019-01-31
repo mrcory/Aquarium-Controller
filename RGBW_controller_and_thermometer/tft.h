dc0f3aa26573ffecb22856da4f98f3f58c3aaaf4
mrcory <33401433+mrcory@users.noreply.github.com>
2019-01-30 20:04:04 -0500

    Fix Merge

--- RGBW_controller_and_thermometer/tft.h
+++ RGBW_controller_and_thermometer/tft.h
@@ -33,12 +33,6 @@ void activeDisplay() {
 
 //---Flop Frame
   if (flipFlop == true) { //Updates even times
-<<<<<<< HEAD
-    ledStatus(0,39);
-    
-    screenReset(72,100,1);
-  
-=======
     ledStatus(0,39,text,backGround);
 
   #if tempWarnEnable //If temperature warning is enabled draw a warning icon.
@@ -55,7 +49,6 @@ void activeDisplay() {
     }
   #endif
     
->>>>>>> a7bcf2b923fe0736ebf819255145f93533021c8a
     flipFlop = !flipFlop;
   }
 
@@ -72,33 +65,17 @@ if (screenFirstRun == true) {
     display.drawBitmap(90,105,waterMap,30,15,ST7735_WHITE); //Draw a water icon. Will display arrow above when draing or filling
   #endif
   
-<<<<<<< HEAD
-  if (screenFirstRun == true) {
-    screenReset(0,100,1);
-    display.drawBitmap(0,108,blynk_small,31,32,ST7735_WHITE);
-    screenFirstRun = false;
-  }
-=======
   screenFirstRun = false;
 }
->>>>>>> a7bcf2b923fe0736ebf819255145f93533021c8a
 
 #if wifiEnable
   static bool oldBlynk = false;
   if (Blynk.connected() == true && oldBlynk == false) {
-<<<<<<< HEAD
-    blynkLogo(133,92,0x2632);
-    oldBlynk = true;
-    } else {
-  if (Blynk.connected() == false && oldBlynk == true) {
-    blynkLogo(133,92,ST77XX_BLACK);
-=======
     blynkLogo(130,90,0x2632);
     oldBlynk = true;
     } else {
   if (Blynk.connected() == false && oldBlynk == true) {
     blynkLogo(130,90,ST77XX_BLACK);
->>>>>>> a7bcf2b923fe0736ebf819255145f93533021c8a
     oldBlynk = false;
   }
   }
