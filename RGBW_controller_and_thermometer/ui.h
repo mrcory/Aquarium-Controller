01cd0302185f6c00c49bcd2063c71b52bc9b4b3d
mrcory <33401433+mrcory@users.noreply.github.com>
2019-12-04 16:40:51 -0500

    Setting change and comment additions

--- RGBW_controller_and_thermometer/ui.h
+++ RGBW_controller_and_thermometer/ui.h
@@ -2,12 +2,12 @@
 //Great color picker.
 //http://www.barth-dev.de/online/rgb565-color-picker/
 
-#define progressFill 0xFFFF
-#define progressEdge 0xFFFF
+#define progressFill 0xFFFF  //Fill for progress bars
+#define progressEdge 0xFFFF  //Top and bottom edges of progress bars
 
 #define backGround 0x0000
-#define text 0xFFFF
+#define text 0xFFFF          //General text color
 
-#define warnColor 0xF800
+#define warnColor 0xF800     //Warning icon color
 
-#define iconColor 0xFFFF
+#define iconColor 0x3771     //Statis icon color
